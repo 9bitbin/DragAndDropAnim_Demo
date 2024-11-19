cb0a1f9a71e2d14fb57d3a06aa9dbbf04036a290
Himal <104921088+9bitbin@users.noreply.github.com>
2024-11-19 15:18:52 -0500

    To Do 1 is complete - done

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -1,5 +1,7 @@
 package edu.farmingdale.draganddropanim_demo
-
+// Himal Shrestha
+// Alrajab
+// BCS 371 - Mobile Application Development
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -30,16 +32,25 @@ class MainActivity : ComponentActivity() {
 
 
 
-// This should be completed in a group setting
+// This should be completed in a group setting \/
 // ToDo 1: Analyze the requirements for Individual Project 3
+
 // ToDo 2: Show the DragAndDropBoxes composable
+
 // ToDo 3: Change the circle to a rect
+
 // ToDo 4: Replace the command right with a image or icon
+
 // ToDo 5: Make this works in landscape mode only
+
 // ToDo 6: Rotate the rect around itself
+
 // ToDo 7: Move - translate the rect horizontally and vertically
+
 // ToDo 8: Add a button to reset the rect to the center of the screen
+
 // ToDo 9: Enable certain animation based on the drop event (like up or down)
+
 // ToDo 10: Make sure to commit for each one of the above and submit this individually
 
 
